d8e041c3c3ba8cf48bdbe5514eee8988b6c3647c
sunshine2329 <seniordeveloper00001@gmail.com>
2023-09-03 19:01:40 -0400

    fix: store custom keys in the jwt

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -41,5 +41,30 @@ export const authOptions: NextAuthOptions = {
         }
       }
     })
-  ]
+  ],
+  callbacks: {
+    session: ({ session, token }) => {
+      console.log('Session Callback', { session, token })
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          randomKey: token.randomKey
+        }
+      }
+    },
+    jwt: ({ token, user }) => {
+      console.log('JWT Callback', { token, user })
+      if (user) {
+        const u = user as unknown as any
+        return {
+          ...token,
+          id: u.id,
+          randomKey: u.randomKey
+        }
+      }
+      return token
+    }
+  }
 }
