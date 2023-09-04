993f79d7e2701a26374d82de1fd89b86962608bf
sunshine2329 <seniordeveloper00001@gmail.com>
2023-09-04 05:36:49 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+
+ // Add basePath
+  basePath: '/github-pages',
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs']
